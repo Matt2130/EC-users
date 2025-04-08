8fc67b64864e0d429901fb9c85d1ccb91c415010
Matt2130 <manuel.hdz.rubio07@gmail.com>
2025-04-07 19:46:17 -0600

    DB connection PORT

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import 'dotenv/config';
 import app from './src/app.js';
 
-const PORT = process.env.PORT_EXPRESS;
+/* const PORT = process.env.PORT_EXPRESS;
+ */
+const PORT = process.env.DB_PORT;
 
 app.listen(PORT, () => {
     console.log(`Servicio en el puerto ${PORT}`)
